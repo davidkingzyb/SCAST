aee9c89234c9fb002c10780d9347a98c3b46af61
DKZ <davidkingzyb@163.com>
2024-12-30 19:27:38 +0800

    js estree d3 ok

---
+++ test/test.js
@@ -0,0 +1,82 @@
+class Animal {
+    constructor(name, species) {
+        this.name = name;
+        this.species = species;
+    }
+
+    makeSound() {
+        console.log(`${this.name} makes a sound`);
+    }
+}
+
+class Dog extends Animal {
+    constructor(name, breed) {
+        super(name, 'Dog'); 
+        this.breed = breed;
+    }
+
+    makeSound() {
+        console.log(`${this.name} barks`);
+    }
+
+    fetch() {
+        console.log(`${this.name} is fetching a ball`);
+    }
+}
+
+const myDog = new Dog('Buddy', 'Golden Retriever');
+
+myDog.makeSound(); 
+
+console.log(myDog.breed); 
+
+myDog.fetch(); 
+
+
+
+function testES5(t){
+
+    if(t=='for'){
+        for(i=0; i<=10; i++){
+            if(i%2==1)continue
+            console.log('for',i)
+        }
+    }else if(t=='while'){
+        let i=0;
+        while(true){
+            i++
+            if(i>10)break
+        }
+    }else if(t=='do'){
+        do{
+            console.log(a)
+        }while(a<10); 
+    }else{
+        console.log('other')
+    }
+    
+    switch(t){
+        case "forin":
+            for(let k in {'a':'aa','b':'bb','c':'cc'}){
+                console.log(k)
+            }
+            break
+        case "forof":
+            for (let x of ['a','b','c']) {
+                console.log(x);
+            }
+            break
+        default:
+            console.log('switch default t')
+    }
+
+    try{
+        throw new Error('test throw')
+    }catch(err){
+        console.warn('err',err)
+    }
+
+    return 'ok'
+}
+
+testES5('for')
